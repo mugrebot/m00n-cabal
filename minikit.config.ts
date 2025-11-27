4ff59439fa6638ebeb78ed56e76f1bfcf5ff7a7a
Mugrebot <mikeongay@gmail.com>
2025-11-26 21:13:19 -0500

    yeet

---
+++ minikit.config.ts
@@ -0,0 +1,10 @@
+import type { MiniKitConfig } from '@coinbase/onchainkit/miniapp';
+
+const config: MiniKitConfig = {
+  miniapp: {
+    name: 'm00nad cabal check',
+    url: 'https://m00nad.vercel.app/miniapp'
+  }
+};
+
+export default config;
