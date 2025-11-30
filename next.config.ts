b592cee540cabf0a90e2d5a65b8c7b2aabe39862
Mugrebot <mikeongay@gmail.com>
2025-11-29 22:24:17 -0500

    config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   experimental: {
-    serverExternalPackages: ['@envio-dev/hypersync-client']
+    serverComponentsExternalPackages: ['@envio-dev/hypersync-client']
   }
 };
 
